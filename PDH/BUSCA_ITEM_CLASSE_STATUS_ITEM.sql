0d785913692b564474df2c3b0571f2aea0eaf4e7
natanzeraa <contato@natanfelipe.com.br>
2025-11-06 17:36:42 -0300

    refactor(pdh): move BUSCA_ITEM_CLASSE_STATUS_ITEM.sql para PDH/

--- PDH/BUSCA_ITEM_CLASSE_STATUS_ITEM.sql
+++ PDH/BUSCA_ITEM_CLASSE_STATUS_ITEM.sql
@@ -1,10 +1,27 @@
 -- BUSCA ITEM CATEGORIA CLASSE E FORNECEDOR COM STATUS (ATIVO ou INATIVO)
 SELECT
-class_tl.description AS CLASSE_DO_ITEM,
-iop.organization_code AS ORGANIZACAO,
-esib.inventory_item_status_code AS STATUS_DO_ITEM,
-esib.item_number AS CODIGO_DO_ITEM,
-esiv.description AS DESCRICAO_DO_ITEM
+  class_tl.description AS CLASSE_DO_ITEM,
+  iop.organization_code AS ORGANIZACAO,
+  case
+    when esib.inventory_item_status_code = 'Active' then 'ATIVO'
+    else 'INATIVO'
+  end as STATUS_DE_INVENTARIO,
+  case
+    when esib.approval_status = 'A' then 'APROVADO'
+    else 'REPROVADO'
+  end as STATUS_APROVACAO,
+  esib.item_number AS CODIGO_DO_ITEM,
+  esiv.description AS DESCRICAO_DO_ITEM,
+  esib.last_updated_by as QUEM_ATUALIZOU,
+  to_char(
+    from_tz(cast(esib.last_update_date as timestamp), 'utc') at time zone 'america/sao_paulo',
+    'dd/mm/yyyy hh24:mi:ss'
+  ) as ATUALIZADO_EM,
+  esib.created_by as QUEM_CRIOU,
+  to_char(
+    from_tz(cast(esib.creation_date as timestamp), 'utc') at time zone 'america/sao_paulo',
+    'dd/mm/yyyy hh24:mi:ss'
+  ) as CRIADO_EM
 FROM
   egp_system_items_b esib
   JOIN egp_system_items_vl esiv ON esiv.inventory_item_id = esib.inventory_item_id
@@ -15,7 +32,8 @@ FROM
   AND class_tl.language = 'PTB'
 where
   1 = 1
---  and esib.inventory_item_status_code = 'Inactive'
+  and esib.approval_status = 'A' -- and esib.inventory_item_status_code = 'Inactive'
+  -- and esiv.description like '%BAG VACINA%'
 ORDER BY
   esib.item_number
 ;
