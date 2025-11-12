edcc65f4d7a462f64ce0466a3bcc816f3608f85c
Natan Oliveira <contato@natanfelipe.com.br>
2025-11-12 15:58:14 -0300

    Adiciona CLIFOR/QUEM_MEXEU_NO_MEU_FORNCEDOR.sql (auditoria de mudanças em fornecedores)

---
+++ CLIFOR/QUEM_MEXEU_NO_MEU_FORNCEDOR.sql
@@ -0,0 +1,38 @@
+SELECT
+  hp.party_name AS supplier_name,
+  ps.segment1 AS supplier_number,
+  ps.creation_source,
+  ps.vendor_type_lookup_code,
+  ps.created_by as CRIADO_POR,
+  to_char(
+    from_tz(cast(ps.creation_date as TIMESTAMP), 'UTC') at time zone 'America/Sao_Paulo',
+    'DD/MM/YYYY HH24:MI:SS'
+  ) as DaTA_CRIACAO,
+  ps.last_updated_by as QUEM_ATUALIZOU,
+  to_char(
+    from_tz(cast(ps.last_update_date as timestamp), 'UTC') at time zone 'America/Sao_Paulo',
+    'DD/MM/YYYY HH24:MI:SS'
+  ) as DATA_ATUALIZACAO    
+FROM
+  poz_suppliers ps
+  JOIN hz_parties hp ON ps.party_id = hp.party_id
+ORDER BY
+  ps.last_update_date DESC
+;
+  
+-- BUSCA TODAS AS COLUNAS PRESENTES NA TABELA
+SELECT
+  column_name,
+  data_type,
+  data_length,
+  data_precision,
+  data_scale,
+  nullable,
+  data_default
+FROM
+  all_tab_columns
+WHERE
+  table_name = 'POZ_SUPPLIERS'
+ORDER BY
+  column_id
+;
