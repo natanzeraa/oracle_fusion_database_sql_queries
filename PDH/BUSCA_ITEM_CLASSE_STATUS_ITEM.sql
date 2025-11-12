fa1ecf90cc8dd7927178104512b7316d8f7a52a9
Natan Oliveira <contato@natanfelipe.com.br>
2025-11-12 15:56:20 -0300

    Inclui consulta adicional e ajusta comentários em PDH/BUSCA_ITEM_CLASSE_STATUS_ITEM.sql

--- PDH/BUSCA_ITEM_CLASSE_STATUS_ITEM.sql
+++ PDH/BUSCA_ITEM_CLASSE_STATUS_ITEM.sql
@@ -32,7 +32,8 @@ FROM
   AND class_tl.language = 'PTB'
 where
   1 = 1
-  and esib.approval_status = 'A' -- and esib.inventory_item_status_code = 'Inactive'
+  and esib.approval_status = 'A' 
+  -- and esib.inventory_item_status_code = 'Inactive'
   -- and esiv.description like '%BAG VACINA%'
 ORDER BY
   esib.item_number
@@ -63,3 +64,36 @@ from
   )
   
 ;
+
+
+-- BUSCA ITEM CATEGORIA CLASSE E FORNECEDOR COM STATUS (ATIVO ou INATIVO)
+
+SELECT  
+  iop.organization_code AS ORGANIZACAO,
+  esib.item_number AS CODIGO_DO_ITEM,
+  esiv.description AS DESCRICAO_DO_ITEM,
+  class_tl.description AS CLASSE_DO_ITEM,
+  case
+    when esib.inventory_item_status_code = 'Active' then 'ATIVO'
+    else 'INATIVO'
+  end as STATUS    
+FROM
+  egp_system_items_b esib
+  JOIN egp_system_items_vl esiv ON esiv.inventory_item_id = esib.inventory_item_id
+  AND esiv.organization_id = esib.organization_id
+  JOIN inv_org_parameters iop ON iop.organization_id = esib.organization_id
+  JOIN egp_item_classes_b class ON esib.item_catalog_group_id = class.item_class_id
+  JOIN egp_item_classes_tl class_tl ON class.item_class_id = class_tl.item_class_id
+  AND class_tl.language = 'PTB'
+where
+  1 = 1
+  --and esib.inventory_item_status_code = 'Active'
+  and esib.item_number = '05072839'
+  -- and esiv.description like '%BAG VACINA%'
+ORDER BY
+  esib.item_number
+
+
+
+
+  
