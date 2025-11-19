6cf6f2dade494891bf191ec34d304d5798d4e814
Natan Felipe <contato@natanfelipe.com.br>
2025-11-19 18:16:12 -0300

    chore: ajustes recentes SQL

--- CARGAS/CARGAS___BUSCA_CARGA_DE_CONTATO_ENDERECO_FORNECEDOR.sql
+++ CARGAS/CARGAS___BUSCA_CARGA_DE_CONTATO_ENDERECO_FORNECEDOR.sql
@@ -29,7 +29,7 @@ from
 where
   1 = 1
   and trunc(from_tz(cast(pscai.creation_date as timestamp), 'utc') at time zone 'america/sao_paulo') between to_date('19/11/2025', 'dd/mm/yyyy') and to_date('19/11/2025', 'dd/mm/yyyy')  
-  -- and pscai.import_status = 'REJECTED'        
+  and pscai.import_status = 'PROCESSED'          
 order by
   pscai.creation_date desc
-  
+
